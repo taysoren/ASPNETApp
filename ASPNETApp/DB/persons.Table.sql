cfa7b9f14694b6b7c79b28e370397355b779e865
Taysoren <taysoren@gmail.com>
2016-10-26 20:22:28 -0600

    Initial Creation

---
+++ ASPNETApp/DB/persons.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [Address] VARCHAR(50) NOT NULL, 
+    [Interests] VARCHAR(50) NOT NULL, 
+    [Picture] VARBINARY(50) NULL
+)
